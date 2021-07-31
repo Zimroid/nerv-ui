9560e9403717a7f68c9f66a7219ac73a2d7b661b
Zimroid <zimroid@gmail.com>
2021-07-31 22:13:00 +0200

    Fix svg display on Firefox

--- src/Connector/Connector.tsx
+++ src/Connector/Connector.tsx
@@ -21,9 +21,19 @@ export default function Connector({text, id, disabled, checked, connectorColor =
     }
 
     useEffect(() => {
-        val.current = checked;
         if (val.current !== checked) {
             inited.current = true;
+        }
+        val.current = checked;
+        if (inited.current) {
+            if (checked) {
+                // TODO : Find a way to correctly type the element in order to use "beginElement"
+                const svg: any = document.getElementById(`animCloseTempo${id}`) as any;
+                svg.beginElement();
+            } else {
+                const svg: any = document.getElementById(`animOpenTempo${id}`) as any;
+                svg.beginElement();
+            }
         }
     }, [checked]);
 
@@ -42,7 +52,41 @@ export default function Connector({text, id, disabled, checked, connectorColor =
                 <div className={styles.cable}>
                     {/* TODO subdivide into 2 path so we can have a different color for the left and right "cable" */}
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 100">
-                        <path stroke={connectorColor} strokeWidth="2" fill="none" vectorEffect="non-scaling-stroke"/>
+                        <path id={'cableConnector' + id} d="M 49 50 L 90 50 C 105 50 105 50 120 50 L 170 50 M 751 50 L 710 50 C 695 50 695 50 680 50 L 630 50" stroke={connectorColor} strokeWidth="2" fill="none" vectorEffect="non-scaling-stroke"/>
+                        <animate
+                            id={'animOpenTempo' + id}
+                            href={'#cableConnector' + id}
+                            attributeName="d"
+                            from=   "M 49 50 L 90 50 C 105 50 105 50 120 50 L 170 50 M 751 50 L 710 50 C 695 50 695 50 680 50 L 630 50"
+                            to=     "M 49 50 L 90 50 C 105 50 105 50 120 50 L 170 50 M 751 50 L 710 50 C 695 50 695 50 680 50 L 630 50"
+                            dur="0.8s"
+                            begin="indefinite"/>
+                        <animate
+                            id={'animOpen' + id}
+                            href={'#cableConnector' + id}
+                            attributeName="d"
+                            from=   "M 49 50 L 90 50 C 105 50 105 50 120 50 L 170 50 M 751 50 L 710 50 C 695 50 695 50 680 50 L 630 50"
+                            to=     "M 49 50 L 70 50 C 85 -20 85 120 100 50 L 170 50 M 751 50 L 730 50 C 715 -20 715 120 700 50 L 630 50"
+                            dur="0.2s"
+                            begin={'animOpenTempo' + id + '.end'}
+                            fill="freeze"/>
+                        <animate
+                            id={'animCloseTempo' + id}
+                            href={'#cableConnector' + id}
+                            attributeName="d"
+                            from=   "M 49 50 L 70 50 C 85 -20 85 120 100 50 L 170 50 M 751 50 L 730 50 C 715 -20 715 120 700 50 L 630 50"
+                            to=     "M 49 50 L 70 50 C 85 -20 85 120 100 50 L 170 50 M 751 50 L 730 50 C 715 -20 715 120 700 50 L 630 50"
+                            dur="0.8s"
+                            begin="indefinite"/>
+                        <animate
+                            id={'animClose' + id}
+                            href={'#cableConnector' + id}
+                            attributeName="d"
+                            from=   "M 49 50 L 70 50 C 85 -20 85 120 100 50 L 170 50 M 751 50 L 730 50 C 715 -20 715 120 700 50 L 630 50"
+                            to=     "M 49 50 L 90 50 C 105 50 105 50 120 50 L 170 50 M 751 50 L 710 50 C 695 50 695 50 680 50 L 630 50"
+                            dur="0.2s"
+                            begin={'animCloseTempo' + id + '.end'}
+                            fill="freeze"/>
                     </svg>
                 </div>
             </label>
